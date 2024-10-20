dc2e1ed5c48640c3daf3d1c7bb1c81696a9b214e
m_sasaki <m_sasaki@m-sasakinonotobukkukonpyuta.local>
2024-10-20 11:02:56 +0900

    ContentViewを実験的に書き換えた2

--- SpeechApp/ContentView.swift
+++ SpeechApp/ContentView.swift
@@ -1,77 +1,25 @@
-//
-//  ContentView.swift
-//  SpeechApp
-//
-//  Created by 松田陵佑 on 2024/10/20.
-//
-
 import SwiftUI
 import CoreData
 
 /*
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+            VStack {
+                NavigationLink(destination: ThemeSelectionView()) {
+                    Text("好きなテーマ")
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .padding()
+                
+                NavigationLink(destination: YouTubeView()) {
+                    Text("YouTube")
                 }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                .padding()
+                
+                NavigationLink(destination: NewsView()) {
+                    Text("News")
                 }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                .padding()
             }
         }
     }
@@ -122,13 +70,22 @@ struct NewsView: View {
 
 
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-#Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct ThemeSelectionView: View {
+    var body: some View {
+        Text("好きなテーマ画面")
+    }
 }
+
+struct YouTubeView: View {
+    var body: some View {
+        Text("YouTube画面")
+    }
+}
+
+struct NewsView: View {
+    var body: some View {
+        Text("News画面")
+    }
+}
+
+
