9cc69eab7af2d422228d9c97c992780a92dc4d15
volume-Volume <137908407+volume-Volume@users.noreply.github.com>
2024-10-20 15:41:34 +0900

    Initial Commit

---
+++ Speaking/SpeakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakingApp.swift
+//  Speaking
+//
+//  Created by 松田陵佑 on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SpeakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeakingTests/SpeakingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakingTests.swift
+//  SpeakingTests
+//
+//  Created by 松田陵佑 on 2024/10/20.
+//
+
+import Testing
+@testable import Speaking
+
+struct SpeakingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
